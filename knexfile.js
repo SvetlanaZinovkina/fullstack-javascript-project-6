7dedc70ca8e19812dc28fbd4c0d082723e3a6153
Svetlana <pidgin66@gmail.com>
2024-06-04 10:33:53 +0300

    add postgressql

--- knexfile.js
+++ knexfile.js
@@ -31,11 +31,8 @@ export const test = {
 export const production = {
   client: 'pg',
   connection: {
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
+    connectionString: process.env.DB_CONNECTION,
+    ssl: { rejectUnauthorized: false },
   },
   useNullAsDefault: true,
   migrations,
