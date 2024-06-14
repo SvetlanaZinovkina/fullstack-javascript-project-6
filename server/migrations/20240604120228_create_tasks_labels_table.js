d373ec83d532141b16e368ef504fbf1d8328a80b
Svetlana <pidgin66@gmail.com>
2024-06-14 22:09:29 +0300

    fix eslint errors

--- server/migrations/20240604120228_create_tasks_labels_table.js
+++ server/migrations/20240604120228_create_tasks_labels_table.js
@@ -1,14 +1,14 @@
 // @ts-check
 
 export const up = (knex) => (
-		knex.schema.createTable('tasks_labels', (table) => {
-				table.increments('id').primary();
-				table.integer('task_id').unsigned().references('id').inTable('tasks')
-						.onDelete('CASCADE');
-				table.integer('label_id').unsigned().references('id').inTable('labels')
-						.onDelete('CASCADE');
-				table.unique(['task_id', 'label_id']);
-		})
+  knex.schema.createTable('tasks_labels', (table) => {
+    table.increments('id').primary();
+    table.integer('task_id').unsigned().references('id').inTable('tasks')
+      .onDelete('CASCADE');
+    table.integer('label_id').unsigned().references('id').inTable('labels')
+      .onDelete('CASCADE');
+    table.unique(['task_id', 'label_id']);
+  })
 );
 
 export const down = (knex) => knex.schema.dropTable('tasks_labels');
